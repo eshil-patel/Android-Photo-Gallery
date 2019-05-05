3c7465a8c5f74be09f9d87319c1de1caaf67387d
khanquer97 <is325@scarletmail.rutgers.edu>
2019-05-05 13:02:52 -0400

    changed to table

--- AndroidProject16/app/src/main/java/com/example/androidproject16/open_album.java
+++ AndroidProject16/app/src/main/java/com/example/androidproject16/open_album.java
@@ -13,10 +13,13 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
 
 import java.net.URI;
+import java.util.ArrayList;
 
-public class open_album extends AppCompatActivity {
+public class open_album extends AppCompatActivity implements View.OnClickListener {
     public static User user;
     public static Album album;
     public static final int photo_request_code=1;
@@ -29,7 +32,7 @@ public class open_album extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_open_album);
         // will have initialize the view for whatever photos there are, and then give options
-        updatelayout();
+        updateLayout();
     }
     public void addPhoto(View view){
         Intent intent = new Intent(Intent.ACTION_PICK);
@@ -50,11 +53,12 @@ public class open_album extends AppCompatActivity {
                     Photo newphoto=new Photo(path);
                     album.addPhoto(newphoto);
                     DataSaver.save(open_album.this,user);
-                    updatelayout();
+//                    updatelayout();
+                    updateLayout();
                 }
             }
         }
-        public void updatelayout(){
+/*        public void updatelayout(){
             //clear the linearlayout first
             LinearLayout gallery = findViewById(R.id.displayphotos);
             gallery.removeAllViews();
@@ -69,6 +73,48 @@ public class open_album extends AppCompatActivity {
                 gallery.addView(image);
 
             }
+        }*/
+        public void updateLayout(){
+            TableLayout grid = findViewById(R.id.imgTable);
+            ArrayList<Photo> temp;
+            temp=album.getPhotos();
+            grid.removeAllViews();
+            int r = 0;
+            int j = 0;
+            TableRow row = new TableRow(this);
+            System.out.println("WIDTH = " + grid.getWidth());
+            int m = grid.getWidth();
+            TableRow.LayoutParams lp = new TableRow.LayoutParams(m/3,m/3,TableRow.LayoutParams.WRAP_CONTENT);
+            row.setLayoutParams(lp);
+            row.setId(r);
+
+            for (Photo i: temp){
+                if (j == 3){
+                    grid.addView(row,r);
+                    r++;
+                    j = 0;
+                    row = new TableRow(this);
+                    row.setId(r);
+                    row.setLayoutParams(lp);
+                }
+                System.out.println("Image "+i);
+                Photo toDisplay= i;
+                ImageView image = new ImageView(this);
+                Bitmap bitmap = BitmapFactory.decodeFile(toDisplay.getPath());
+                image.setImageBitmap(bitmap);
+                image.setLayoutParams(lp);
+                image.setId(3*r + j);
+                image.setOnClickListener(this);
+                row.addView(image,j);
+                j++;
+            }
+            grid.addView(row,r);
+        }
+        @Override
+        public void onClick(View v){
+            int id = v.getId();
+            System.out.println(id);
+
         }
     }
 
