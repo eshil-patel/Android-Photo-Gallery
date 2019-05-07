bffa931f41b184b0f080a420100c5fc96eabc079
khanquer97 <is325@scarletmail.rutgers.edu>
2019-05-07 11:30:43 -0400

    has some errors

--- AndroidProject16/app/src/main/java/com/example/androidproject16/display_Photo.java
+++ AndroidProject16/app/src/main/java/com/example/androidproject16/display_Photo.java
@@ -6,16 +6,22 @@ import android.provider.ContactsContract;
 import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 
 public class display_Photo extends AppCompatActivity {
     public static User user;
     public static Album album;
     public static int currentImg = -1;
+    public static int currentTag = -1;
+    public static ArrayAdapter<String> adapter;
 
 
     public static void init(User u,String a,int cImg){
@@ -42,25 +48,25 @@ public class display_Photo extends AppCompatActivity {
         Bitmap bitmap = BitmapFactory.decodeFile(toDisplay.getPath());
         m.setImageBitmap(bitmap);
 
-        EditText t1n = findViewById(R.id.tag1Name);
-        EditText t1v = findViewById(R.id.tag1Value);
-        EditText t2n = findViewById(R.id.tag2Name);
-        EditText t2v = findViewById(R.id.tag2Value);
+        EditText t1n = findViewById(R.id.tagNameText);
+        EditText t1v = findViewById(R.id.tagValueText);
 
         t1n.setText("");
         t1v.setText("");
-        t2n.setText("");
-        t2v.setText("");
 
 
         if (toDisplay.getTags().size() >= 1){
             t1n.setText(toDisplay.getTags().get(0).getName());
             t1v.setText(toDisplay.getTags().get(0).getValue());
         }
-        if (toDisplay.getTags().size() == 2){
-            t2n.setText(toDisplay.getTags().get(1).getName());
-            t2v.setText(toDisplay.getTags().get(1).getValue());
-        }
+
+        ListView tagList = findViewById(R.id.tagList);
+
+
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_expandable_list_item_1,toDisplay.getTags().toString());
+        tagList.setAdapter(adapter);
+        adapter.notifyDataSetChanged();
+
         System.out.println(toDisplay.getTags());
     }
     public void activateButtons(){
@@ -85,50 +91,15 @@ public class display_Photo extends AppCompatActivity {
         updateScreen();
         activateButtons();
     }
-    public void editTags(View view){
-        Photo photo= album.getPhoto(currentImg);
 
-        EditText t1n = findViewById(R.id.tag1Name);
-        EditText t1v = findViewById(R.id.tag1Value);
-        EditText t2n = findViewById(R.id.tag2Name);
-        EditText t2v = findViewById(R.id.tag2Value);
+    public void addTag(View view){
 
-        if(t2n.getText().toString().isEmpty() ){
-            System.out.println("REMOVE TAG 1");
-            photo.removeTags(1);
-        }
-        if (t1n.getText().toString().isEmpty()){
-            System.out.println("REMOVE TAG 0");
-            photo.removeTags(0);
-            return;
-        }
-        if (t1n.getText().toString().equalsIgnoreCase("person") || t1n.getText().toString().equalsIgnoreCase("location")){
-            if (t1v.getText().toString().isEmpty()){
-                showAlert("TAG VALUE MUST BE NONNULL");
-                return;
-            }
-            photo.editTag(0,t1n.getText().toString(),t1v.getText().toString());
-            System.out.println("TAG 1 ADDED");
-            DataSaver.save(this,user);
-        }else{
-            showAlert("TAGS MUST BE 'PERSON' OR 'LOCATION'");
-            return;
-        }
-        if(t2n.getText().toString().isEmpty()){
-            return;
-        }
-        if (t2n.getText().toString().equalsIgnoreCase("person") || t2n.getText().toString().equalsIgnoreCase("location")){
-            if (t2v.getText().toString().isEmpty()){
-                showAlert("TAG VALUE MUST BE NONNULL");
-                return;
-            }
-            photo.editTag(1,t2n.getText().toString(),t2v.getText().toString());
-            System.out.println("TAG 2 ADDED");
-            DataSaver.save(this,user);
-        }else{
-            showAlert("TAGS MUST BE 'PERSON' OR 'LOCATION'");
-            return;
-        }
+    }
+    public void deleteTag(View view){
+
+    }
+    public void moveAlbum(View view){
+
     }
     public void showAlert(String text){
         AlertDialog.Builder alert = new AlertDialog.Builder(this);
