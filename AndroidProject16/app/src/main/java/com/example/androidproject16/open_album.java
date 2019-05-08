176444df2964d79c169fa7e7152b54702e82c069
Eshil Patel <xeshil101@optonline.net>
2019-05-08 09:04:36 -0400

    final

--- AndroidProject16/app/src/main/java/com/example/androidproject16/open_album.java
+++ AndroidProject16/app/src/main/java/com/example/androidproject16/open_album.java
@@ -152,9 +152,9 @@ public class open_album extends AppCompatActivity implements View.OnClickListene
             case 1: {
                 if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                         updateLayout();
-
-                } else {
-                    Toast.makeText(open_album.this, "Permission denied", Toast.LENGTH_SHORT).show();
+                }
+                else {
+                    Toast.makeText(open_album.this, "Permission denied, please allow permissions", Toast.LENGTH_SHORT).show();
                 }
                 return;
             }
